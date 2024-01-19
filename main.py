4291dedc78261504d0e7117a3ca3aafd274c888c
Student <mtrejo.student@montessorischool.es>
2024-01-19 11:29:39 +0100

    update Member from datatype to class

--- main.py
+++ main.py
@@ -1,27 +1,50 @@
-from dataclasses import dataclass
+SILVER = 2500
+GOLD = 3000
+PLATINUM = 4000
 
-@dataclass
-class Members:
-    ID: str
-    Surname: str
-    YearJoined: int
-    MemberType: str
-    NightsBooked: int
-    Points: int
+class Member:
+    def __init__(self, id, surname, year_joined, member_type, nights_booked, points):
+        self.__ID = id
+        self.__Surname = surname
+        self.__YearJoined = year_joined
+        self.__MemberType = member_type
+        self.__NightsBooked = nights_booked
+        self.__Points = points
+
+
+    def GetMemberType(self):
+        return self.__MemberType
+
+    def SetMemberType(self):
+        if self.__NightsBooked >= 30 and self.__NightsBooked<100:
+            self.__MemberType = "Gold"
+        elif self.__NightsBooked >= 100:
+            self.__MemberType = "Platinum"
+
+    def GetNightsBooked(self):
+        return self.__NightsBooked
+
+    def SetNightsBooked(self, newNights):
+        self.__NightsBooked = self.__NightsBooked + newNights
+
+    def SetPoints(self, newNights):
+        if self.__MemberType == "Silver":
+            self.__Points = self.__Points + SILVER*newNights
+        elif self.__MemberType == "Gold":
+            self.__Points = self.__Points + GOLD*newNights
+        else:
+            self.__Points = self.__Points + PLATINUM*newNights
+
+    def GetPoints(self):
+        return self.__Points
 
 
 members = []
-
 def readMembers():
     file = open("SampleData2017.txt", "r")
     for line in file:
         newMember = line.strip()
         newMember = newMember.split(",")
-        members.append(Members(newMember[0], newMember[1], int(newMember[2]), newMember[3], int(newMember[4]), int(newMember[5])))
-
-def showMembers():
-    for i in range(len(members)):
-        print(f"{members[i].ID} {members[i].Surname:<9} {members[i].YearJoined} {members[i].MemberType:<8} {members[i].NightsBooked:<4} {members[i].Points}")
+        members.append(Member(newMember[0], newMember[1], int(newMember[2]), newMember[3], int(newMember[4]), int(newMember[5])))
 
 readMembers()
-showMembers()
