25f538e549c3fcb6aaa5a9111c8daf3b50f9314e
Student <mtrejo.student@montessorischool.es>
2024-01-24 10:39:39 +0100

    added main program commands

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+import random
+
+
+
 SILVER = 2500
 GOLD = 3000
 PLATINUM = 4000
@@ -12,8 +17,17 @@ class Member:
         self.__Points = points
 
 
+    def GetID(self):
+        return self.__ID
+
+    def GetSurname(self):
+        return self.__Surname
+
     def GetMemberType(self):
         return self.__MemberType
+
+    def GetYearJoined(self):
+        return self.__YearJoined
 
     def SetMemberType(self):
         if self.__NightsBooked >= 30 and self.__NightsBooked<100:
@@ -46,5 +60,58 @@ def readMembers():
         newMember = line.strip()
         newMember = newMember.split(",")
         members.append(Member(newMember[0], newMember[1], int(newMember[2]), newMember[3], int(newMember[4]), int(newMember[5])))
+
+def newMemeber():
+    surname = input("Please enter your surname \n>")
+    newID = surname[0:2] + str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(datetime.now().year)[2:3]
+    members.append(Member(newID, surname, datetime.now().year, "Silver", 0, 0))
+
+def newBooking():
+    ID = input("Please enter your ID \n>")
+    i = 0
+    Found = False
+    while not Found and i <= len(members):
+        if ID == Member.GetID(members[i]):
+            Found = True
+
+    if Found:
+        numberOfNights = 0
+        while numberOfNights <= 0 or numberOfNights > 14:
+            numberOfNights = int(input("How many nights would you like to book? (1-14) \n>"))
+        Member.SetNightsBooked(members[i], numberOfNights)
+        Member.SetPoints(members[i], numberOfNights)
+
+    else:
+        print("Member not found")
+
+def MemberInfo():
+    ID = input("Please enter your ID \n>")
+    i = 0
+    Found = False
+    while not Found and i <= len(members):
+        if ID == Member.GetID(members[i]):
+            Found = True
+
+    if Found:
+        print(f" Member ID: {Member.GetID(members[i])}, Surname: {Member.GetSurname(members[i])}, Year joined: {Member.GetYearJoined(members[i])}, Membership type: {Member.GetMemberType(members[i])}, Number of nights booked: {Member.GetNightsBooked(members[i])}, Current Points: {Member.GetPoints(members[i])}")
+
+    else:
+        print("Member not found")
+
+
+more = True
+while more:
+    action = input("Would you like to add a member(1), book more nights(2) or display the information of a member(3)? \n>")
+    if action == "1":
+        newMemeber()
+        repeat = None
+        while repeat!="Y" or repeat!="N":
+            repeat = input("Would you like to do something else? (Y/N) \n>")
+
+
+    elif action == "2":
+        newBooking()
+    elif action == "3":
+        MemberInfo()
 
 readMembers()
